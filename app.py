33cf2ca47e2242d6d0c558460bef74abaa0e4dbe
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-05 00:59:26 +0900

    Add More Fields for Music API
    Add title, artist and mood field for the response

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#
+# Flask server module
+#
 from main import predict
 from flask import Flask, jsonify, request
 
@@ -21,18 +24,33 @@ def hello_world():
 def music():
   return jsonify(
     {
+      "title": "Intro And Tarantelle",
+      "artist": "ABC",
+      "mood": 'regret',
       "url": "http://www.openmusicarchive.org/audio/Intro_And_Tarantelle.mp3",
     },
     {
+      "title": "Dont Go Way Nobody",
+      "artist": "EFG",
+      "mood": 'alone',
       "url": "http://www.openmusicarchive.org/audio/Dont_Go_Way_Nobody.mp3"
     },
     {
+      "title": "April Kisses",
+      "artist": "HIJ",
+      "mood": 'sad',
       "url": "http://www.openmusicarchive.org/audio/April_Kisses.mp3"
     },
     {
+      "title": "Eddies Twister",
+      "artist": "KLM",
+      "mood": 'relaxed',
       "url": "http://www.openmusicarchive.org/audio/Eddies_Twister.mp3"
     },
     {
+      "title": "Little Bits",
+      "artist": "NOP",
+      "mood": 'happy',
       "url": "http://www.openmusicarchive.org/audio/Little_Bits.mp3"
     }
   )
