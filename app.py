a0c6d745aaeac65f3d0251510465b14126851f5f
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-05 01:25:17 +0900

    Add Cors to Flask

--- app.py
+++ app.py
@@ -3,9 +3,11 @@
 #
 from main import predict
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 
 app = Flask(__name__, static_folder="public", static_url_path="/")
+CORS(app)
 
 
 @app.route("/")
