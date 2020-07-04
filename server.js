b1151fd31d876c91dc17486a6557b7e9300926d4
xANARKYx <shikhars3145@gmail.com>
2020-07-04 19:10:31 +0530

    modeled response

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const router = require("./routes");
+app.use(cors());
+const router = require('./routes');
 
 // Use environment variable PORT or 3000 if not defined.
 const port = process.env.PORT || 3000;
 
 // Serve static contents from static directory.
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Use API endpoint from routes module.
 app.use(router);
 
 // Test API endpoint.
-app.get("/hello", (req, res) => {
-  res.send("Hello, World!");
+app.get('/hello', (req, res) => {
+  res.send('Hello, World!');
 });
 
 // Start listening for requests.
