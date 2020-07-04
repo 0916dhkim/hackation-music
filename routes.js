32b13be03cac0af6070c2ca8688a55aad4dc7e4e
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-04 19:29:14 +0900

    Add Placeholder Music

--- routes.js
+++ routes.js
@@ -6,8 +6,18 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/music", (req, res) => {
+  const candidates = [
+    "http://www.openmusicarchive.org/audio/Intro_And_Tarantelle.mp3",
+    "http://www.openmusicarchive.org/audio/Dont_Go_Way_Nobody.mp3",
+    "http://www.openmusicarchive.org/audio/ancoats/Local%20Recall%20-%20Eileen%20Simpson%20and%20Ben%20White%20with%20live%20soundtrack%20by%20Graham%20Massey.mp3",
+    "http://www.openmusicarchive.org/audio/April_Kisses.mp3",
+    "http://www.openmusicarchive.org/audio/Eddies_Twister.mp3",
+    "http://www.openmusicarchive.org/audio/Little_Bits.mp3",
+    "http://www.openmusicarchive.org/audio/Struggling.mp3",
+    "http://www.openmusicarchive.org/audio/Umbrellas_To_Mend.mp3"
+  ];
   res.send({
-    result: "OK"
+    url: candidates[Math.floor(Math.random() * candidates.length)]
   });
 });
 
