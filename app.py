2bd58e992fbb3f8590b6bfa69326e1ede4d11071
xANARKYx <shikhars3145@gmail.com>
2020-07-05 16:18:15 +0530

    handled mood input in backend

--- app.py
+++ app.py
@@ -27,13 +27,51 @@ def hello_world():
 # Return an array of music URL
 @app.route("/music")
 def music():
-  return jsonify([
+  mood = request.args.get('mood') #get mood from query string
+
+  if mood is None: # if no mood specified play relaxing songs only
+    return jsonify([
     {
       "title": track.title,
       "artist": track.artist,
-      "url": track.preview
+      "url": track.preview,
+      "relaxing": True
     } for track in relaxingTracks
   ])
+
+  # number of non relaxing songs as per given mood
+  sadNo = {
+  'love': 0,
+  'lonely': 2,
+  'sad': 1,
+  'happy': 0,
+  'anxious': 3,
+  'relaxed': 0 ,
+  }
+
+  setSize = sadNo[mood] # size of sad sons array
+
+  nonRel = [
+    {
+      "title": track.title,
+      "artist": track.artist,
+      "url": track.preview,
+      "relaxing": False
+    } for track in random.sample(nonRelaxingTracks, setSize) # create a random set of non relaxing songs of size setSize
+  ]  
+
+  Rel = [
+    {
+      "title": track.title,
+      "artist": track.artist,
+      "url": track.preview,
+      "relaxing": True
+    } for track in relaxingTracks
+  ]
+
+  res = nonRel+Rel # join nonrelaxing and relaxing tracks in response
+
+  return jsonify(res)
 
 
 # Determine if "text" is relaxing or not.
